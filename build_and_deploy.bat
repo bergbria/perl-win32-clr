05e9b878f9d45c962c5f111ff69831a5aaa70a19
Brian Berger <bberger@microsoft.com>
2019-05-03 16:38:29 -0700

    Add build_and_deploy.bat

---
+++ build_and_deploy.bat
@@ -0,0 +1,19 @@
+@echo off
+setlocal
+pushd %~dp0
+
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\Tools\VsDevCmd.bat" -arch=amd64
+del clr.c
+del CLR.obj
+nmake
+nmake install
+D:\temp\copyclr.bat
+
+copy c:\perl64\site\lib\auto\Win32\CLR\CLR.dll %PkgPerl%\site\lib\auto\Win32\CLR\CLR.dll
+copy c:\perl64\site\lib\auto\Win32\CLR\CLR.exp %PkgPerl%\site\lib\auto\Win32\CLR\CLR.exp
+copy c:\perl64\site\lib\auto\Win32\CLR\CLR.lib %PkgPerl%\site\lib\auto\Win32\CLR\CLR.lib
+copy c:\perl64\site\lib\auto\Win32\CLR\CLR.pdb %PkgPerl%\site\lib\auto\Win32\CLR\CLR.pdb
+copy c:\perl64\html\site\lib\Win32\CLR.html %PkgPerl%\html\site\lib\Win32\CLR.html
+copy c:\perl64\site\lib\Win32\CLR.pm %PkgPerl%\site\lib\Win32\CLR.pm
+
+endlocal
