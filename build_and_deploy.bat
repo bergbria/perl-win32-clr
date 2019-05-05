1e0fc4784f0141e57e0bbae8f00256cfb2a9fdd6
Brian Berger <bberger@microsoft.com>
2019-05-05 13:35:18 -0700

    Streamline makefile; support using %pkgperl% instead of c:\perl64

--- build_and_deploy.bat
+++ build_and_deploy.bat
@@ -2,18 +2,7 @@
 setlocal
 pushd %~dp0
 
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\Tools\VsDevCmd.bat" -arch=amd64
-rem del clr.c
-rem del CLR.obj
-nmake
+call "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Enterprise\Common7\Tools\VsDevCmd.bat" -arch=amd64
 nmake install
-
-rem D:\temp\copyclr.bat
-copy c:\perl64\site\lib\auto\Win32\CLR\CLR.dll %PkgPerl%\site\lib\auto\Win32\CLR\CLR.dll
-copy c:\perl64\site\lib\auto\Win32\CLR\CLR.exp %PkgPerl%\site\lib\auto\Win32\CLR\CLR.exp
-copy c:\perl64\site\lib\auto\Win32\CLR\CLR.lib %PkgPerl%\site\lib\auto\Win32\CLR\CLR.lib
-copy c:\perl64\site\lib\auto\Win32\CLR\CLR.pdb %PkgPerl%\site\lib\auto\Win32\CLR\CLR.pdb
-copy c:\perl64\html\site\lib\Win32\CLR.html %PkgPerl%\html\site\lib\Win32\CLR.html
-copy c:\perl64\site\lib\Win32\CLR.pm %PkgPerl%\site\lib\Win32\CLR.pm
 
 endlocal
